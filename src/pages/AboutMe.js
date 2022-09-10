35a524824df85f4d6d046fb7d4c058588f9463ba
CJSantee <santeedev@gmail.com>
2022-09-10 12:26:29 -0500

    Updated content

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,27 +1,34 @@
 // Bootstrap Components
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 // API Route
-const API_URL = "https://radiant-ravine-94842.herokuapp.com/resume"
+const API_URL = "https://radiant-ravine-94842.herokuapp.com/resume";
 
 function AboutMe() {
-    return (
-        <Row className="section justify-content-center" id="AboutMe">
-            <Col md>
-                <h2 className="text-primary">About Me</h2>
-                <p>Hi! I'm Colin.</p>
-                <p>I'm a creative and technically minded student inspired by software solutions with significant societal impact. Passionate about projects in web engineering, mobile applications, and full-stack development.</p>
-                <Button variant="outline-primary" href={API_URL}>Resume</Button>
-            </Col>
-            <Col md>
-                <Row className="justify-content-center">
-                    <div className="graphic" />
-                </Row>
-            </Col>
+  return (
+    <Row className='section justify-content-center' id='AboutMe'>
+      <Col md>
+        <h2 className='text-primary'>About Me</h2>
+        <p>Hi! I'm Colin.</p>
+        <p>
+          I'm a creative and technically minded software developer inspired by
+          software solutions with significant societal impact. I'm passionate
+          about projects in web engineering, mobile applications, and full-stack
+          development.
+        </p>
+        <Button variant='outline-primary' href={API_URL}>
+          Resume
+        </Button>
+      </Col>
+      <Col md>
+        <Row className='justify-content-center'>
+          <div className='graphic' />
         </Row>
-    );
+      </Col>
+    </Row>
+  );
 }
 
 export default AboutMe;
